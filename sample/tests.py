bc24784d80fcf1627b59b0e2af83a7fa0fed18da
luismasuelli <luismasuelli@hotmail.com>
2016-08-03 00:15:52 -0500

    also tested CourseSpec's hierarchy

--- sample/tests.py
+++ sample/tests.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from arcanelab.ouroboros.executors import Workflow
-from arcanelab.ouroboros.models import NodeSpec, WorkflowSpec
+from arcanelab.ouroboros.models import NodeSpec, WorkflowSpec, CourseSpec
 from arcanelab.ouroboros import exceptions
 
 """
@@ -551,3 +551,152 @@ class CourseSpecTestCase(ValidationErrorWrappingTestCase):
         exc = self.unwrapValidationError(ar.exception)
         self.assertEqual(exc.code, exceptions.WorkflowCourseSpecHasNoRequiredNode.CODE,
                          'Invalid subclass of ValidationError raised')
+
+    def test_bad_caller_type_is_bad(self):
+        try:
+            Workflow.Spec.install({'model': 'sample.Task',
+                                   'code': 'wf-badnode',
+                                   'name': 'Workflow Spec',
+                                   'description': 'This workflow spec shall not pass',
+                                   'create_permission': '',
+                                   'cancel_permission': '',
+                                   'courses': [{
+                                       'code': '',
+                                       'name': 'Root',
+                                       'description': 'The root course',
+                                       'nodes': [{
+                                           'type': NodeSpec.ENTER,
+                                           'code': 'origin',
+                                           'name': 'Origin',
+                                           'description': 'The origin node'
+                                       }, {
+                                           'type': NodeSpec.SPLIT,
+                                           'code': 'parallel-1',
+                                           'name': 'Parallel',
+                                           'joiner': 'sample.support.dummy_joiner',
+                                           'description': 'Parallel brancher',
+                                           'branches': ['foo', 'bar']
+                                       }, {
+                                           'type': NodeSpec.EXIT,
+                                           'code': 'exit',
+                                           'name': 'Exit',
+                                           'description': 'One exit node',
+                                           'exit_value': 100,
+                                       }, {
+                                           'type': NodeSpec.EXIT,
+                                           'code': 'exit-2',
+                                           'name': 'Exit 2',
+                                           'description': 'Another exit node',
+                                           'exit_value': 100,
+                                       }, {
+                                           'type': NodeSpec.CANCEL,
+                                           'code': 'cancel',
+                                           'name': 'Cancel',
+                                           'description': 'The cancel node',
+                                       }],
+                                       'transitions': [{
+                                           'origin': 'origin',
+                                           'destination': 'parallel-1',
+                                           'name': 'Initial transition',
+                                           'description': 'The initial transition',
+                                           'permission': 'sample.start_task',
+                                       }, {
+                                           'origin': 'parallel-1',
+                                           'action_name': 'escape-1',
+                                           'destination': 'exit',
+                                           'name': 'Final transition 1',
+                                           'description': 'The final transition to exit 1',
+                                       }, {
+                                           'origin': 'parallel-1',
+                                           'action_name': 'escape-2',
+                                           'destination': 'exit-2',
+                                           'name': 'Final transition 2',
+                                           'description': 'The final transition to exit 2',
+                                       }]
+                                   }, {
+                                       'code': 'foo',
+                                       'name': 'Foo',
+                                       'description': 'Foo branch',
+                                       'nodes': [{
+                                           'type': NodeSpec.ENTER,
+                                           'code': 'origin',
+                                           'name': 'Origin',
+                                           'description': 'The origin node'
+                                       }, {
+                                           'type': NodeSpec.EXIT,
+                                           'code': 'exit',
+                                           'name': 'Exit',
+                                           'description': 'The only exit node',
+                                           'exit_value': 100,
+                                       }, {
+                                           'type': NodeSpec.CANCEL,
+                                           'code': 'cancel',
+                                           'name': 'Cancel',
+                                           'description': 'The cancel node',
+                                       }, {
+                                           'type': NodeSpec.JOINED,
+                                           'code': 'joined',
+                                           'name': 'Joined',
+                                           'description': 'The joined node',
+                                       }],
+                                       'transitions': [{
+                                           'origin': 'origin',
+                                           'destination': 'exit',
+                                           'name': 'Initial transition',
+                                           'description': 'The initial and only transition',
+                                           'permission': 'sample.start_task',
+                                       }]
+                                   }, {
+                                       'code': 'bar',
+                                       'name': 'Bar',
+                                       'description': 'Bar branch',
+                                       'nodes': [{
+                                           'type': NodeSpec.ENTER,
+                                           'code': 'origin',
+                                           'name': 'Origin',
+                                           'description': 'The origin node'
+                                       }, {
+                                           'type': NodeSpec.EXIT,
+                                           'code': 'exit',
+                                           'name': 'Exit',
+                                           'description': 'The only exit node',
+                                           'exit_value': 100,
+                                       }, {
+                                           'type': NodeSpec.CANCEL,
+                                           'code': 'cancel',
+                                           'name': 'Cancel',
+                                           'description': 'The cancel node',
+                                       }, {
+                                           'type': NodeSpec.JOINED,
+                                           'code': 'joined',
+                                           'name': 'Joined',
+                                           'description': 'The joined node',
+                                       }],
+                                       'transitions': [{
+                                           'origin': 'origin',
+                                           'destination': 'exit',
+                                           'name': 'Initial transition',
+                                           'description': 'The initial transition',
+                                           'permission': 'sample.start_task',
+                                       }]
+                                   }]})
+            node_spec = NodeSpec.objects.get(code='parallel-1', course_spec__code='',
+                                             course_spec__workflow_spec__code='wf-badnode')
+            node_spec.type = NodeSpec.INPUT
+            node_spec.save()
+            course_spec = CourseSpec.objects.get(code='foo', workflow_spec__code='wf-badnode')
+        except Exception as e:
+            self.assertFalse(True, 'An exception was raised (%s): %s' % (type(e).__name__, e))
+        else:
+            with self.assertRaises(ValidationError) as ar:
+                course_spec.full_clean()
+            exc = self.unwrapValidationError(ar.exception)
+            self.assertEqual(exc.code, exceptions.WorkflowCourseSpecHasInvalidCallers.CODE,
+                             'Invalid subclass of ValidationError raised')
+
+    def tearDown(self):
+        """
+        Removing the successfully created workflow spec.
+        """
+
+        WorkflowSpec.objects.all().delete()
